691aa7d175558c64ddf1c318eb0fb2cc5cd4a168
TechEdzprogrammer <113817954+TechEdzprogrammer@users.noreply.github.com>
2026-04-25 09:49:53 +0800

    change link of basic-theme-website

--- theme-links.js
+++ theme-links.js
@@ -1,5 +1,5 @@
 window.TECH_EDZ_THEME_LINKS = {
-  signature: "https://wedding-invitation-basic-theme.netlify.app/",
+  signature: "https://wedding-invitation-basic.netlify.app/",
   personal: "https://wedding-invitation-personal-website.netlify.app/",
   booklet: "https://wedding-invitaion-booklet.netlify.app/",
   classic: "https://wedding-invitation-classic.netlify.app/"
